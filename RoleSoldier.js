e6e4272fff794927b99e67da2709c931528f0a37
kennyhcy <35054956+kennyhcy@users.noreply.github.com>
2021-03-08 11:00:16 +0800

    Update RoleSoldier.js

--- RoleSoldier.js
+++ RoleSoldier.js
@@ -1,1 +1,41 @@
+var CONSTS = require('Sys').CONSTS;
+var roleSoldier = {
+    run: function (creep) {
+        this[creep.memory.role].run(creep);
+    },
 
+    [CONSTS.SOLDIER_ROLE_TANK]:
+    {
+        new: function () { },
+        run: function () { },
+    },
+
+    [CONSTS.SOLDIER_ROLE_COMMANDO]:
+    {
+        new: function () { },
+        run: function () { },
+    },
+
+    [CONSTS.SOLDIER_ROLE_SHOOTER]:
+    {
+        new: function () { },
+        run: function () { },
+    },
+
+    [CONSTS.SOLDIER_ROLE_ARTILLERY]:
+    {
+        new: function () { },
+        run: function () { },
+    },
+
+    [CONSTS.SOLDIER_ROLE_MEDIC]:
+    {
+        new: function () { },
+        run: function () { },
+    },
+
+
+}
+
+
+module.exports = roleSoldier;
