05da1555f7c393cd1cc63dd30e1f6f051b94cb35
kennyhcy <35054956+kennyhcy@users.noreply.github.com>
2021-03-09 11:29:01 +0800

    Update BaseTower.js

--- BaseTower.js
+++ BaseTower.js
@@ -1,5 +1,5 @@
 // BaseTower.js
-// 2021-03-09 08:52
+// 2021-03-09 11:25
 
 var CONSTS = require('Sys').CONSTS;
 
@@ -11,33 +11,29 @@ var baseTower = {
 
     [CONSTS.TOWER_ROLE_NORMAL]:
     {
-        run: function (ttower) {
-            var tower = Game.structures[ttower.id];
+        run: function (tower) {
+            //var tower = Game.structures[ttower.id];
             var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if (target) {
                 tower.attack(target);
             }
 
             if (!target) {
-                var filter_my_structure = {
+                var target = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return structure.hits < structure.hitsMax
                     }
-                };
-
-                var filter_basic_structure = {
-                    filter: (structure) => {
-                        return (
-                            structure.structureType == STRUCTURE_ROAD
-                            || structure.structureType == STRUCTURE_CONTAINER
-                            || structure.structureType == STRUCTURE_STORAGE
-                        ) && structure.hits < structure.hitsMax
-                    }
-                };
-
-                var target = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, filter_my_structure);
+                });
                 if (!target) {
-                    target = tower.pos.findClosestByRange(FIND_STRUCTURES, filter_basic_structure);
+                    target = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            return (
+                                structure.structureType == STRUCTURE_ROAD
+                                || structure.structureType == STRUCTURE_CONTAINER
+                                || structure.structureType == STRUCTURE_STORAGE
+                            ) && structure.hits < structure.hitsMax
+                        }
+                    });
                 }
 
                 if (target) {
